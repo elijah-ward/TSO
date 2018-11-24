6e5f9e4f8209321b161c2420d5bcf435fa8085a8
kvande85 <kvande85@uwo.ca>
2018-11-24 15:55:31 -0500

    Observation_block created. Need info from schema to fill out

--- tsocli/tsocli/fixed_constraint.py
+++ tsocli/tsocli/fixed_constraint.py
@@ -1,6 +1,6 @@
 # Define a class
 class FixedConstraint:
+    type = "Fixed"
     def __init__(self, weight, timeHorizon):
     	self.weight = weight	
     	self.timeHorizon = timeHorizon
-
--- tsocli/tsocli/test.py
+++ tsocli/tsocli/test.py
@@ -1,6 +1,9 @@
 import fixed_constraint
 import observation_block
 import dynamic_constraint
+import transformer
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 
 dConstraint = dynamic_constraint.DynamicConstraint(2590, 2)
@@ -10,6 +13,10 @@ fConstraint = fixed_constraint.FixedConstraint(22, 22.0)
 print("fixed constraint: ", fConstraint.weight, fConstraint.timeHorizon)
 
 constraints = [fConstraint, dConstraint]
+c = SkyCoord(ra=10.625*u.degree, dec=41.2*u.degree, frame='icrs')
 
-block = observation_block.ObservationBlock(22, "Kirk", 10, 0.0001, constraints, 200) 
-print("Observation block: ", block.researcher_name, block.priority, block.coordinates, block.constraints, block.duration)
+block = observation_block.ObservationBlock(22, "Kirk", 10, c, constraints) 
+print("Observation block: ", block.researcher_name, block.priority, block.coordinates, block.constraints[0].type, block.constraints[1].type)
+
+newBlock = transformer.transform("testing")
+print("New Block, researcher name: ", newBlock.researcher_name)
