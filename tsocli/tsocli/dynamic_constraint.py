2c07b5f6dd0e6450b6e41ec7129a3dc6bb284e30
kvande85 <kvande85@uwo.ca>
2018-11-24 14:12:01 -0500

    started constraints and observation block

---
+++ tsocli/tsocli/dynamic_constraint.py
@@ -0,0 +1,6 @@
+# Define a class
+class DynamicConstraint:
+    def __init__(self, weight, confidence):
+    	self.weight = weight
+    	self.confidence = confidence
+
---
+++ tsocli/tsocli/observation_block.py
@@ -0,0 +1,7 @@
+class ObservationBlock:
+	def __init__(self,	id, researcher_name, priority, coordinates, constraints):
+		self.researcher_name = researcher_name
+		self.priority = priority
+		self.coordinates = coordinates
+		self.constraints = constraints
+		
