289ae8a36024bfb0ba91551220c712311f5b06a4
Elijah Ward <elijahward.dev@gmail.com>
2019-01-10 11:39:21 -0500

    advance ga.py according to deap docs

--- src/tso/scheduler/ga.py
+++ src/tso/scheduler/ga.py
@@ -4,19 +4,24 @@ ga.py
 Much of the following implementation is explained at https://deap.readthedocs.io/en/master/tutorials/basic/part1.html
 """
 
+from deap import base
+from deap import creator
 from deap import tools
+
 from .cost_heuristic import CostHeuristic
 
-IND_SIZE = 10
-
-
-def ga():
+def ga(n_timeslots, n_pop, n_gen, cxpb, mutpb, eval_func, time_assignment, observation_blocks):
 
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMax)
 
     toolbox = base.Toolbox()
-    toolbox.register("attribute", random.random)
-    toolbox.register("individual", tools.initRepeat, creator.Individual,
-                     toolbox.attribute, n=IND_SIZE)
+    toolbox.register("attr_timeslot", random.randint, 0, n_timeslots - 1)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_timeslot, len(observation_blocks))
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    # Genetic Operators
+    toolbox.register("evaluate", evalOneMax)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
