ece829f5fcab1eb86b7c14d896c97816f3e6b060
Elijah Ward <elijahward.dev@gmail.com>
2019-03-21 01:52:21 -0400

    pull out runtime config values into fake 'config' object in __main__.py, dynamically generate FixedTargets for demo

--- src/tso/scheduler/__main__.py
+++ src/tso/scheduler/__main__.py
@@ -1,8 +1,18 @@
 from tso.scheduler import scheduler
 
+config = {
+    'slew_rate': 0.8,
+    'filters': {
+        'filter': {('MSE', 'EXAMPLE'): 10 }
+    }
+}
+
 def main():
     print('Inside the main Scheduler Application')
-    scheduler.generate_schedule(0)
+
+    start_datetime = '2019-03-01 19:00'
+    end_datetime = '2019-03-13 19:00'
+    scheduler.generate_schedule(config, start_datetime, end_datetime)
 
 
 
--- src/tso/scheduler/scheduler.py
+++ src/tso/scheduler/scheduler.py
@@ -8,34 +8,47 @@ from astropy import units as u
 from tso.importer import data_importer as di
 from tso.scheduler import constraint_aggregator as ca
 
+import random
 
-def create_transitioner(slew_rate):
-    return Transitioner(slew_rate,
-                        {'filter': {('B', 'G'): 10 * u.second,
-                                    ('G', 'R'): 10 * u.second,
-                                    'default': 30 * u.second}})
+def create_transitioner(slew_rate, filters):
+    # Apply atropy units to all config values
+    slew_rate = slew_rate * u.deg / u.second
+    for key in filters['filter'].keys():
+        filters['filter'][key] = filters['filter'][key] * u.second
+    return Transitioner(slew_rate, filters)
 
 
-def generate_schedule(schedule_horizon):
+def generate_schedule(config, start_datetime, end_datetime, ):
 
     print('Inside scheduler.py')
-
     download_IERS_A()
-
     cfht = Observer.at_site('cfht')
-    deneb = FixedTarget.from_name('Deneb')
-    m13 = FixedTarget.from_name('M13')
+    transitioner = create_transitioner(config['slew_rate'], config['filters'])
 
-    noon_before = Time('2019-03-11 19:00')
-    noon_after = Time('2019-03-12 19:00')
+    # Placeholder -- generating demonstration targets
+    n_targets_test = 30
+    target_names = ['Deneb', 'M13', 'sirius', 'M31', 'Polaris', 'Vega']
+    targets = []
 
+    # dynamically produce a list of FixedTarget objects from the target_names identifiers
+    # This is just a placeholder until we figure out properly creating targets from coords
+
+    for i in range(0,n_targets_test):
+        targets.append(FixedTarget.from_name(target_names[random.randrange(len(target_names))]))
+
+    # Retrieve global constraints from Constraint Aggregator
     global_constraints = ca.initialize_constraints()
 
     # requests = di.get_observations_with_constraint(min_priority=99)
 
+    # These are hard
     read_out = 20 * u.second
     n_exp = 5
+
     blocks = []
+
+    # Hardcoded test constraints -- may have some on the ObservingBlock level
+    # These differ from global_constraints above which are applied to all observations in the schedule
 
     half_night_start = Time('2019-03-12 02:00')
     half_night_end = Time('2019-03-12 08:00')
@@ -57,26 +70,31 @@ def generate_schedule(schedule_horizon):
     deneb_exp = 100 * u.second
     m13_exp = 50 * u.second
 
-    for priority, bandpass in enumerate(['B', 'G', 'R']):
-        # We want each filter to have separate priority (so that target
-        # and reference are both scheduled)
-        b = ObservingBlock.from_exposures(deneb, priority, deneb_exp, n_exp, read_out,
-                                          configuration={'filter': bandpass},
-                                          constraints=[first_half_night])
-        blocks.append(b)
-        b = ObservingBlock.from_exposures(m13, priority, m13_exp, n_exp, read_out,
-                                          configuration={'filter': bandpass},
-                                          constraints=[first_half_night])
-        blocks.append(b)
+    for target in targets:
+        for priority, bandpass in enumerate(['MSE']):
+            block = ObservingBlock.from_exposures(target, priority, deneb_exp, n_exp, read_out,
+                                            configuration={'filter': bandpass},
+                                            constraints=[])
+            blocks.append(block)
+            print('Appending block with Target {} and filter {}'.format(target, bandpass))
 
-    slew_rate = .8 * u.deg / u.second
-    transitioner = create_transitioner(slew_rate)
+    # for priority, bandpass in enumerate(['B', 'G', 'R']):
+    #     # We want each filter to have separate priority (so that target
+    #     # and reference are both scheduled)
+    #     b = ObservingBlock.from_exposures(deneb, priority, deneb_exp, n_exp, read_out,
+    #                                       configuration={'filter': bandpass},
+    #                                       constraints=[])
+    #     blocks.append(b)
+    #     b = ObservingBlock.from_exposures(m13, priority, m13_exp, n_exp, read_out,
+    #                                       configuration={'filter': bandpass},
+    #                                       constraints=[])
+    #     blocks.append(b)
 
-    prior_scheduler = SequentialScheduler(constraints=global_constraints,
+    prior_scheduler = SequentialScheduler(constraints=[],
                                           observer=cfht,
                                           transitioner=transitioner)
 
-    priority_schedule = Schedule(noon_before, noon_after)
+    priority_schedule = Schedule(Time(start_datetime), Time(end_datetime))
 
     prior_scheduler(blocks, priority_schedule)
 
@@ -84,4 +102,4 @@ def generate_schedule(schedule_horizon):
 
 
 if '__name__' == '__main__':
-    generate_schedule(0)
+    generate_schedule(None)
