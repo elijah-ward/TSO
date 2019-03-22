6164a5e92a9f104f2c9228068b823444ee2cd34a
gaunsd <gam0929@gmail.com>
2019-03-22 02:59:58 -0400

    Added styling to HTML export

--- src/tso/exporter/exporter.py
+++ src/tso/exporter/exporter.py
@@ -8,9 +8,30 @@ The more complex case is exporting it to a reusable file for CFHT Schedulers
 """
 
 import pandas
+from astropy.table.jsviewer import DEFAULT_CSS
+
+custom_css = """
+body::before {
+    content: "TSO | Observe Freely";
+    position: absolute;
+    top: 0;
+    font-size: 50px;
+    margin-top: 25px;
+    margin-left: 40px;
+    font-weight: bold;
+    font-family: monospace;
+}
+body {
+    margin-top: 150px;
+    background-color: rgb(230, 230, 230);
+    margin-left: 30px;
+}
+"""
+
 
 def export_to_console(schedule):
     print(schedule.to_table())
+
 
 def export_to_file(schedule):
     print("Printing schedule to file...")
@@ -18,8 +39,13 @@ def export_to_file(schedule):
     schedule_df = schedule_table.to_pandas()
     schedule_df.to_csv('./schedule.csv')
 
+
 def export_to_browser(schedule):
     print("Displaying schedule in browser...")
     schedule_table = schedule.to_table()
-    schedule_table.show_in_browser()
+    schedule_table.show_in_browser(
+        jsviewer=True,
+        css=DEFAULT_CSS+custom_css
+    )
+    # there is also show_in_notebook if we want to use an ipy thing
 
