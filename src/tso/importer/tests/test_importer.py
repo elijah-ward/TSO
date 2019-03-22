0658e2de05e24ada8bdbcef140463c41c96a6eff
gaunsd <gam0929@gmail.com>
2019-03-21 20:35:18 -0400

    Refactoring out Custom SQL Stuff
    * Removed our own tables from the SQL so that we will be handling pure CFHT data
    * modified relevant entities like (generate test data, data importer, and tests)
    * next step is to transform this to the observation request

--- src/tso/importer/tests/test_importer.py
+++ src/tso/importer/tests/test_importer.py
@@ -10,7 +10,7 @@ class TestDataImporter:
         assert len(observations) > 0
 
     def test_observation_get_with_constraint(self):
-        observations = data_importer.get_observations_with_constraint(
+        observations = data_importer.get_observations(
             min_priority=0,
             remaining_observing_chances=MAX_SIZE/2,
             observation_duration_min=-1,
@@ -21,7 +21,7 @@ class TestDataImporter:
         assert len(observations) <= len(all_observations)
 
     def test_observation_get_with_constraint_with_default_values(self):
-        observations = data_importer.get_observations_with_constraint()
+        observations = data_importer.get_observations()
         all_observations = data_importer.get_all_observations()
 
         assert len(observations) == len(all_observations)
