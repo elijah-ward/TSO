9ff6e75d5a04f9853c05388b9b5fc3f56547ec47
Elijah Ward <elijahward.dev@gmail.com>
2019-03-22 00:01:43 -0400

    move all exporting functionality to exporter.py

--- src/tso/exporter/exporter.py
+++ src/tso/exporter/exporter.py
@@ -7,11 +7,18 @@ The simple case is exporting to the console.
 The more complex case is exporting it to a reusable file for CFHT Schedulers
 """
 
+import pandas
 
 def export_to_console(schedule):
-    print(schedule)
-
+    print(schedule.to_table())
 
 def export_to_file(schedule):
-    print("Printing To File Now!~")
-    export_to_console(schedule)
+    print("Printing schedule to file...")
+    schedule_table = schedule.to_table()
+    schedule_df = schedule_table.to_pandas()
+    schedule_df.to_csv('./schedule.csv')
+
+def export_to_browser(schedule):
+    print("Displaying schedule in browser...")
+    schedule.show_in_browser()
+
