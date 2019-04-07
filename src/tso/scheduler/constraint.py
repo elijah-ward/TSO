1e1a13b6314e60b4302b6e2e767053a502c331cf
Elijah Ward <elijahward.dev@gmail.com>
2019-04-06 21:30:07 -0400

    delete constraint.py -- this is now implicit in the architecture rather than being divided by modules

--- src/tso/scheduler/constraint.py
+++
@@ -1,7 +0,0 @@
-class Constraint:
-
-    def __init__(self, weight, constraint_type, time_horizon):
-        self.weight = weight
-        self.constraint_type = constraint_type
-        self.timeHorizon = time_horizon
-
